bc2bb3f51311cd2565a6556a8b2f5c999f06527c
Lorenzinco23 <lorenzinco23@gmail.com>
2023-08-03 17:34:33 +0200

    ciao

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use dimacs::parse_dimacs;
+
+pub fn main()-> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    println!("Path given: {:?}", args);
+    let path = &args[1];
+    println!("Path given: {:?}", path);
+
+    //parse the file at path
+    let sat_instance= parse_dimacs(path).unwrap();
+
+    println!("SAT instance: {:?}", sat_instance);
+    
+    Ok(())
 }
+
+
